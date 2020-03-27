0fede9c33467c05d2db7d582f54fb32a429f4d4c
Amit Ganvir <amitganvir6@gmail.com>
2020-03-27 20:40:55 +0530

    updaint zookeeper

--- tfmodules/aws/modules/kafka/variables.tf
+++ tfmodules/aws/modules/kafka/variables.tf
@@ -6,6 +6,7 @@ variable "region" {}
 variable "azs" {
     default = []
 }
+variable "aws_key_name" {}
 
 variable "environment" {}
 
@@ -14,17 +15,31 @@ variable "vpc_id" {}
 variable "aws_pub_subnet_id_str" {}
 variable "vpc_cidr" {}
 
-variable "aws_key_name" {}
-
 variable "kafka_image" {}
 variable "kafka_instance_type" {}
 variable "kafka_instance_count" {}
 variable "kafka_cluster_size" {}
 variable "kafka_lc" {}
-
 // Tags //
 variable "kafka_service_name" {}
+
+
+/*
+   Variables for zookeeper
+*/
+// zookeeper //
+variable "zookeeper_image" {}
+variable "zookeeper_instance_type" {}
+variable "zookeeper_instance_count" {}
+variable "zookeeper_cluster_size" {}
+variable "zookeeper_lc" {}
+variable "zookeeper_sg" {}
+variable "zookeeper_profile_iam_id" {}
+// Tags //
 variable "zookeeper_service_name" {}
-// Route 53 //
+
+/*
+   Variables for Route 53
+*/
 variable "zone_name" {}
 variable "rec_name" {}
