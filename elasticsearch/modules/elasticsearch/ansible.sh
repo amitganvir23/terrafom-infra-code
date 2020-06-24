abd33663f147e2b3df790c5e71ed5b9c0d07701e
Open Source book <amitganvir6@gmail.com>
2020-06-25 02:50:36 +0530

    update

--- elasticsearch/modules/elasticsearch/ansible.sh
+++ elasticsearch/modules/elasticsearch/ansible.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# elasticsearch_plybook="../../../ansible/test/cp-ansible/"
 elasticsearch_plybook="../../../ansible-code/elasticsearch/"
-sleep 2m 
-sudo find  $elasticsearch_plybook -type f|xargs dos2unix
-sudo chmod 655 $elasticsearch_plybook/hosts/*
-sleep 1m
-sudo $elasticsearch_plybook/hosts/ec2.py --list --refresh
+sleep 2m
 cd $elasticsearch_plybook
+sudo find  . -type f|xargs dos2unix
+sudo chmod 655 hosts/*
+sudo hosts/ec2.py --list --refresh
 #sudo ansible-playbook elasticsearch-inv-create.yml -i hosts/ec2.py
-#sleep 40
+sleep 1m
 #ansible-playbook all.yml -i hosts/elasticsearch_inventory > /tmp/elasticsearch-ansible.log
 #ansible-playbook elasticsearch_hosts.yml elasticsearch-deploy.yml -i hosts/ec2.py --private-key /tmp/sai-ec2.pem >> /tmp/elasticsearch-ansible.log
+ansible-playbook -i hosts/ec2.py elasticsearch.yml
